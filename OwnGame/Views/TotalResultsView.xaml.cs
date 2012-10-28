49ab350fbde78fd5fd58f649f2ff492eee6c5fdd
Herman <hermasoft@tut.by>
2012-10-29 00:53:27 +0300

    [Feature]Results view display

--- OwnGame/Views/TotalResultsView.xaml.cs
+++ OwnGame/Views/TotalResultsView.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using OwnGame.Infrastructure;
 
 namespace OwnGame.Views
 {
@@ -21,6 +10,7 @@ namespace OwnGame.Views
     {
         public TotalResultsView()
         {
+            ViewModelLocatorHelper.CreateStaticViewModelLocatorForDesigner(this, new ViewModelLocator());
             InitializeComponent();
         }
     }

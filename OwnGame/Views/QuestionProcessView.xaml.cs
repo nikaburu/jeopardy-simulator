bec91759e8a785820b0e127a3b01fbd1669148f6
Herman Starzhynski <hermasoft@tut.by>
2012-12-17 19:22:25 +0300

    [Feature]Text and image scroll

--- OwnGame/Views/QuestionProcessView.xaml.cs
+++ OwnGame/Views/QuestionProcessView.xaml.cs
@@ -14,5 +14,23 @@ namespace OwnGame.Views
             ViewModelLocatorHelper.CreateStaticViewModelLocatorForDesigner(this, new ViewModelLocator());
             InitializeComponent();
         }
+
+        private void Resizeble_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (textBlock != null)
+            {
+                textBlock.FontSize += e.Delta > 0 ? 1 : -1;
+                return;
+            }
+
+            Image image = sender as Image;
+            if (image != null)
+            {
+                image.Width += e.Delta > 0 ? 1 : -1;
+                image.Height += e.Delta > 0 ? 1 : -1;
+                return;
+            }
+        }
     }
 }
